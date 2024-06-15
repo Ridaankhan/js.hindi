304512c6d105c73c7f80e75bee2a8d272652a8d1
Ridaankhan <khanridaan5@gmail.com>
2024-06-15 08:05:20 +0000

    datatype

---
+++ java/01_basics/01_variable.js
@@ -0,0 +1,19 @@
+ const accountId =8081
+ let accountEmail = "ridaan@gmail.com"
+ var accountPassword = "123456" 
+ accountCity = "mawai"
+
+    
+ //accountId = 2 // not allowed
+
+ accountEmail = "khanridaan@gmail.com"
+ accountPassword = "22022"
+ accountCity = "chauraha"
+ console.log(accountCity);
+
+ console.table([accountId, accountEmail, accountPassword, accountCity]);
+
+ /*
+ prefer not to use var
+ bcz of issue in block scope and functional scope
+ */
---
+++ java/01_basics/2-datatypes.js
@@ -0,0 +1,13 @@
+"use strict" //treat all JS code as newer version
+
+// alert ( 3 + 3)// we are using node js not browser//
+
+//string
+//boolean
+//null // typeof object
+//undefined // typeof undefined
+//object
+//symbol
+//number => 2 to power 53
+console.log( typeof undefined) // undefined
+console.log(typeof null) //object
